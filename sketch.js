5fe9453aaebe29e20cd5d230c739d95b3ba1e421
gusttavoam <101047935+gusttavoam@users.noreply.github.com>
2022-07-17 22:03:49 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let s;
+let scl = 20;
+
+let food;
+
+function setup() {
+  createCanvas(1360, 654);
+  frameRate(10);
+  s = new Snake()
+
+  pickLockation()
+  
+}
+
+function pickLockation(){
+  let cols = floor(width/scl);
+  let rows = floor(height/scl);
+
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl)
+}
+
+function draw() {
+  background(2)
+
+  if(s.eat(food)){
+    pickLockation();
+  }
+  s.death();
+  s.update();
+  s.show();
+
+  fill(0, 100, 0);
+  rect(food.x, food.y, scl, scl)
+  
+}
+
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    s.dir(0, -1);
+  } else if(keyCode === DOWN_ARROW){
+    s.dir(0, 1)
+   }else if(keyCode === RIGHT_ARROW){
+    s.dir(1, 0)
+  } else if(keyCode === LEFT_ARROW){
+    s.dir(-1, 0)
+  }
+}
